a3639d69901ad955bf62ae041fdfcb7b56fc33ff
swathi shri <swathimuthoju092@gmail.com>
2026-02-10 14:35:24 +0530

    added class leatcodePerfectnumber

---
+++ src/com/javaprograms/LeatcodeperfectNumber.java
@@ -0,0 +1,18 @@
+package com.javaprograms;
+
+public class LeatcodeperfectNumber {
+    public static boolean checkPerfectNumber(int num){
+        if (num <= 1) return false;
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        int num = 28;
+    }
+}
